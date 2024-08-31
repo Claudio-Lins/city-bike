10c1d8f1b94f4835627957c78e5e50b154cd69ff
Claudio Lins <claudio.lins@me.com>
2024-08-31 21:10:21 +0100

    Update bike-map.tsx

--- src/components/bike-map.tsx
+++ src/components/bike-map.tsx
@@ -31,6 +31,7 @@ export function BikeMap() {
   const [numberOfStationsPerNetwork, setNumberOfStationsPerNetwork] = useState<{
     [network: string]: number
   }>({})
+  const [stationDetails, setStationDetails] = useState<any[]>([])
 
   useEffect(() => {
     const fetchNetworkData = async () => {
@@ -86,6 +87,9 @@ export function BikeMap() {
           throw new Error("No stations found in the network.")
         }
 
+        // Atualizar estado com detalhes das estações
+        setStationDetails(response.network.stations)
+
         return response.network.stations.length
       } catch (error: any) {
         console.error(`Error counting stations: ${error.message}`)
@@ -97,7 +101,6 @@ export function BikeMap() {
       [country: string]: number
     }> => {
       try {
-        // Usar fetchDataWithCache para obter dados com cache
         const data = await fetchDataWithCache<NetworksDataTypes>(
           `${process.env.NEXT_PUBLIC_API_URL}/v2/networks`,
           "networksByCountryData",
@@ -126,6 +129,8 @@ export function BikeMap() {
     fetchNetworkData()
     getNetworksByCountry().then((data) => setNetworksByCountry(data))
   }, [])
+
+  console.log("networkData", networkData)
 
   return (
     <MapContainer
@@ -201,27 +206,25 @@ export function BikeMap() {
               add: () => setActiveLayer("stationDetails"),
             }}
           >
-            {networkData?.networks?.map((network) => {
-              const location = network.location
-              return (
-                <Marker
-                  key={network.id}
-                  position={[location.latitude, location.longitude]}
-                  icon={customIcon}
-                >
-                  <Popup>
-                    <div>
-                      <pre>
-                        <strong>Station details:</strong>
-                        <br />
-                        Network ID: {network.id}
-                        <br />
-                      </pre>
-                    </div>
-                  </Popup>
-                </Marker>
-              )
-            })}
+            {stationDetails.map((station) => (
+              <Marker
+                key={station.id}
+                position={[station.latitude, station.longitude]}
+                icon={customIcon}
+              >
+                <Popup>
+                  <div>
+                    <strong>{station.name}</strong>
+                    <p>Available Bikes: {station.free_bikes}</p>
+                    <p>Empty Slots: {station.empty_slots}</p>
+                    <p>
+                      Last Updated:{" "}
+                      {new Date(station.timestamp).toLocaleString()}
+                    </p>
+                  </div>
+                </Popup>
+              </Marker>
+            ))}
           </LayerGroup>
         </LayersControl.Overlay>
       </LayersControl>

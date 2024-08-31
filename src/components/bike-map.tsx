bd4892c4282293eb36f8ba99342e66e80c757cc0
Claudio Lins <claudio.lins@me.com>
2024-09-01 00:50:15 +0100

    Update bike-map.tsx

--- src/components/bike-map.tsx
+++ src/components/bike-map.tsx
@@ -5,16 +5,20 @@ import {
   MapContainer,
   TileLayer,
   Marker,
-  Popup,
   LayersControl,
   LayerGroup,
   useMapEvents,
   Tooltip,
+  useMap,
 } from "react-leaflet"
 import "leaflet/dist/leaflet.css"
 import L from "leaflet"
 import { countryPosition } from "@/utils/countryPosition"
-import { CityBikeNetwork, CityBikeApiResponse } from "@/@types/cityBikeTypes"
+import {
+  CityBikeNetwork,
+  CityBikeApiResponse,
+  CityBikeStation,
+} from "@/@types/cityBikeTypes"
 
 const customIcon = new L.Icon({
   iconUrl:
@@ -32,6 +36,12 @@ export function BikeMap() {
   const [networksInCountry, setNetworksInCountry] = useState<CityBikeNetwork[]>(
     []
   )
+  const [stationsInNetwork, setStationsInNetwork] = useState<CityBikeStation[]>(
+    []
+  )
+
+  const initialCenter: [number, number] = [20, 0] // Centro inicial do mapa
+  const initialZoom = 2 // Zoom inicial do mapa
 
   useEffect(() => {
     async function fetchNetworks() {
@@ -73,26 +83,136 @@ export function BikeMap() {
     }
   }, [selectedCountry])
 
-  const handleCountryClick = (country: string) => {
-    setSelectedCountry(country)
-    setActiveLayer("networksInCountry")
+  const NetworkMarker = ({ network }: { network: CityBikeNetwork }) => {
+    const map = useMap()
+
+    const handleClick = async () => {
+      const response = await fetch(
+        `http://api.citybik.es/v2/networks/${network.id}`
+      )
+      const data = await response.json()
+
+      setStationsInNetwork(data.network.stations)
+      setActiveLayer("stationsInNetwork")
+
+      // Ampliar o mapa e centralizar na posição da rede clicada no L2
+      map.setView([network.location.latitude, network.location.longitude], 10) // Zoom ajustado para centralizar a rede
+    }
+
+    return (
+      <Marker
+        key={network.id}
+        position={[network.location.latitude, network.location.longitude]}
+        icon={customIcon}
+        eventHandlers={{ click: handleClick }}
+      >
+        <Tooltip
+          direction="top"
+          offset={[0, -25]}
+          opacity={1}
+          permanent={false}
+        >
+          <div>
+            <strong>{network.name}</strong>
+            <p>Number of Stations: {network.stations?.length || 0}</p>
+          </div>
+        </Tooltip>
+      </Marker>
+    )
   }
 
-  // Pink: Função para voltar ao L1 quando clicar fora dos markers
+  const CountryMarker = ({
+    country,
+    count,
+  }: {
+    country: string
+    count: number
+  }) => {
+    const map = useMap()
+    const position = countryPosition(country)
+
+    const handleClick = () => {
+      setSelectedCountry(country)
+      setActiveLayer("networksInCountry")
+
+      if (position) {
+        map.setView(position, 5) // Ampliar e centralizar no país no L1
+      }
+    }
+
+    return (
+      <Marker
+        key={country}
+        position={position || [0, 0]}
+        icon={customIcon}
+        eventHandlers={{
+          click: handleClick,
+        }}
+      >
+        <Tooltip
+          direction="top"
+          offset={[0, -25]}
+          opacity={1}
+          permanent={false}
+        >
+          <div>
+            <strong>{country}</strong>
+            <p>Number of Networks: {count}</p>
+          </div>
+        </Tooltip>
+      </Marker>
+    )
+  }
+
   const MapEventsHandler = () => {
+    const map = useMap()
+
     useMapEvents({
       click: () => {
         setSelectedCountry(null)
         setActiveLayer("networksByCountry")
+
+        // Volta o zoom e a posição do mapa para os valores iniciais
+        map.setView(initialCenter, initialZoom)
       },
     })
     return null
   }
 
+  const StationMarker = ({ station }: { station: CityBikeStation }) => {
+    const map = useMap()
+
+    const handleClick = () => {
+      map.setView([station.latitude, station.longitude], 13) // Ampliar menos e centralizar no L3
+    }
+
+    return (
+      <Marker
+        key={station.id}
+        position={[station.latitude, station.longitude]}
+        icon={customIcon}
+        eventHandlers={{ click: handleClick }}
+      >
+        <Tooltip
+          direction="top"
+          offset={[0, -25]}
+          opacity={1}
+          permanent={false}
+        >
+          <div>
+            <strong>{station.name}</strong>
+            <p>Free Bikes: {station.free_bikes}</p>
+            <p>Empty Slots: {station.empty_slots}</p>
+          </div>
+        </Tooltip>
+      </Marker>
+    )
+  }
+
   return (
     <MapContainer
-      center={[20, 0]}
-      zoom={2}
+      center={initialCenter}
+      zoom={initialZoom}
       style={{ height: "100vh", width: "100%", marginTop: -10 }}
     >
       <TileLayer
@@ -100,7 +220,6 @@ export function BikeMap() {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       />
       <MapEventsHandler />{" "}
-      {/* Pink: Adiciona o manipulador de eventos no mapa */}
       <LayersControl position="topright">
         <LayersControl.Overlay
           name="Number of networks, per country"
@@ -111,34 +230,9 @@ export function BikeMap() {
               add: () => setActiveLayer("networksByCountry"),
             }}
           >
-            {Object.entries(networksByCountry).map(([country, count]) => {
-              const position: [number, number] = countryPosition(country) || [
-                0, 0,
-              ]
-
-              return (
-                <Marker
-                  key={country}
-                  position={position}
-                  icon={customIcon}
-                  eventHandlers={{
-                    click: () => handleCountryClick(country),
-                  }}
-                >
-                  <Tooltip
-                    direction="top"
-                    offset={[0, -25]}
-                    opacity={1}
-                    permanent={false}
-                  >
-                    <div>
-                      <strong>{country}</strong>
-                      <p>Number of Networks: {count}</p>
-                    </div>
-                  </Tooltip>
-                </Marker>
-              )
-            })}
+            {Object.entries(networksByCountry).map(([country, count]) => (
+              <CountryMarker key={country} country={country} count={count} />
+            ))}
           </LayerGroup>
         </LayersControl.Overlay>
 
@@ -152,29 +246,22 @@ export function BikeMap() {
                 add: () => setActiveLayer("networksInCountry"),
               }}
             >
-              {networksInCountry.map((network) => {
-                const position: [number, number] = [
-                  network.location.latitude,
-                  network.location.longitude,
-                ]
+              {networksInCountry.map((network) => (
+                <NetworkMarker key={network.id} network={network} />
+              ))}
+            </LayerGroup>
+          </LayersControl.Overlay>
+        )}
 
-                return (
-                  <Marker
-                    key={network.id}
-                    position={position}
-                    icon={customIcon}
-                  >
-                    <Tooltip>
-                      <div>
-                        <strong>{network.name}</strong>
-                        <p>
-                          Number of Stations: {network.stations?.length || 0}
-                        </p>
-                      </div>
-                    </Tooltip>
-                  </Marker>
-                )
-              })}
+        {stationsInNetwork.length > 0 && (
+          <LayersControl.Overlay
+            name="Stations in network"
+            checked={activeLayer === "stationsInNetwork"}
+          >
+            <LayerGroup>
+              {stationsInNetwork.map((station) => (
+                <StationMarker key={station.id} station={station} />
+              ))}
             </LayerGroup>
           </LayersControl.Overlay>
         )}

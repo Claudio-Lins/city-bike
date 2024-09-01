4458088ac43788d3e0f40a8ceeb27dbe656f42bf
Claudio Lins <claudio.lins@me.com>
2024-09-01 17:15:18 +0100

    core: prettier-plugin-tailwindcss

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,20 @@
-"use client"
+"use client";
 
-import { Header } from "@/components/header"
-import dynamic from "next/dynamic"
+import { Header } from "@/components/header";
+import dynamic from "next/dynamic";
 
 const BikeMap = dynamic(
   () => import("@/components/bike-map").then((mod) => mod.BikeMap),
-  { ssr: false }
-)
+  { ssr: false },
+);
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center w-full relative">
+    <main className="relative flex min-h-screen w-full flex-col items-center">
       <Header />
-      <div className="w-full h-full relative z-0">
+      <div className="relative z-0 h-full w-full">
         <BikeMap />
       </div>
     </main>
-  )
+  );
 }
